7004357773d64f713ec321cb8fff17f3c2160609
MassStash <MassStash@gmail.com>
2014-01-26 00:59:26 -0600

    switched to suit local OctoStash builds

--- bscript.sh
+++ bscript.sh
@@ -7,13 +7,13 @@
 PUSH=$1
 BSPEED=$2
 : ${PUSH:=false}
-: ${BSPEED:="21"}
+: ${BSPEED:="10"}
 BVARIANT=$3
 
 source build/envsetup.sh
-source jet/credentials.sh
+#source jet/credentials.sh
 
-echo "Setting Lunch Menu to ${BVARIANT}"
+echo "Setting Lunch Menu to ${BVARIANT} Bitches!!!"
 lunch oct_${BVARIANT}-userdebug
 
 ## Clean Up Previous Builds as well as old md5sum files
@@ -21,6 +21,7 @@ make installclean && rm -rf out/target/product/*/*md5sum
 
 ## Current Build Date
 BDATE=`date +%m-%d`
+OUT=~/AndroidDev/octostash/out
 
 if [ $1 = "y" ]; then
 PUSH=true
@@ -33,9 +34,9 @@ if [ ! -d "${COPY_DIR}/${BDATE}" ]; then
 	mkdir -p ${COPY_DIR}/${BDATE}
 fi
 
-echo "Starting brunch with ${BSPEED} threads for ${COPY_DIR}"
+echo "Starting brunch with ${BSPEED}.5 jigawatts threads for ${COPY_DIR}"
 if ${PUSH}; then
-echo "Pushing to Remote after build!"
+echo "Pushing to Remote after fukin build!"
 fi
 # Build command
 brunch ${BVARIANT} -j${BSPEED}
@@ -44,35 +45,13 @@ find ${OUT} '(' -name 'Oct*' -size +150000 ')' -print0 |
         while read CHECKSUM FILENAME
         do
 		if [ ${FILENAME} == "-" ]; then
-			echo "Borked Build"
+			echo "Fuked Up Build"
 		else
 			if ! $PUSH; then
-			echo "Moving to Copybox"
+			echo "Moving to Copybox G..."
                 	cp ${FILENAME} ${COPY_DIR}/${BDATE}/${FILENAME##*/}
                 	cp "${FILENAME}.md5sum" ${COPY_DIR}/${BDATE}/${FILENAME##*/}.md5
 			fi
-		OTAFILE=`basename ${FILENAME} | cut -f 1 -d '.'`
-		echo "Filename ${FILENAME} - OTAFILE: ${OTAFILE}"
-		if ${PUSH}; then
-			if [ -e ota.xml ]; then
-			    echo "Cleaning old OTA manifest"
-			    rm ota.xml
-			fi
-			echo "Pulling OTA manifest"
-			wget http://www.teamoctos.com/ota.xml
-			echo "Updating manifest for ${OTAFILE}"
-	                sed -i "s/Oct-[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]-${BVARIANT}/${OTAFILE}/g" ota.xml
-     			echo "Removing existing file from remote."
-			sleep 2
-			ssh ${RACF}@${RHOST} "rm -rf ${ROUT}/${BVARIANT}/*.zip" < /dev/null
-			sleep 2
-     			echo "Pushing new file ${OTAFILE} to remote"
-                        scp ${FILENAME} ${RACF}@${RHOST}:${ROUT}/${BVARIANT}
-			echo "Pushing new OTA manifest to remote"
-			scp ota.xml ${RACF}@${RHOST}:public_html/ota.xml
-			echo "Triggering Sync"
-			curl ${RSYNC}
-		fi
-	fi
+	        fi
         done
 
