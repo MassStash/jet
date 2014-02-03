106c3a41802e874e27a5b3fcebfefde352a95c07
MassStash <MassStash@gmail.com>
2014-02-03 00:43:11 -0600

    Freshin up the shite

--- bscript.sh
+++ bscript.sh
@@ -11,10 +11,9 @@ BSPEED=$2
 BVARIANT=$3
 
 source build/envsetup.sh
-#source jet/credentials.sh
 
-echo "Setting Lunch Menu to ${BVARIANT} Bitches!!!"
-lunch oct_${BVARIANT}-userdebug
+echo "Setting Lunch Menu to ${BVARIANT} bidness"
+lunch carbon_${BVARIANT}-userdebug
 
 ## Clean Up Previous Builds as well as old md5sum files
 make installclean && rm -rf out/target/product/*/*md5sum
@@ -29,28 +28,29 @@ else
 PUSH=false
 fi
 
-if [ ! -d "${COPY_DIR}/${BDATE}" ]; then
-	echo "Creating directory for ${COPY_DIR}/${BDATE}"
-	mkdir -p ${COPY_DIR}/${BDATE}
+if [ ! -d "${COPY_DIR}/${BDATE}/${BVARIANT}" ]; then
+	echo "Creating the damn directory for ${COPY_DIR}/${BDATE}/${BVARIANT}"
+	mkdir -p ${COPY_DIR}/${BDATE}/${BVARIANT}
+	chmod 775 ${COPY_DIR}/${BDATE}/${BVARIANT}
 fi
 
-echo "Starting brunch with ${BSPEED}.5 jigawatts threads for ${COPY_DIR}"
+echo "Starting brunch with ${BSPEED}.5 jigawatts of threads for ${COPY_DIR}"
 if ${PUSH}; then
-echo "Pushing to Remote after fukin build!"
+echo "Pushing to Remote after mutha fukin' build!"
 fi
 # Build command
 brunch ${BVARIANT} -j${BSPEED}
-find ${OUT} '(' -name 'Oct*' -size +150000 ')' -print0 |
+find ${OUT} '(' -name 'oct*' -size +150000 ')' -print0 |
         xargs --null md5sum |
         while read CHECKSUM FILENAME
         do
 		if [ ${FILENAME} == "-" ]; then
-			echo "Fuked Up Build"
+			echo "Borked Build"
 		else
 			if ! $PUSH; then
-			echo "Moving to Copybox G..."
-                	cp ${FILENAME} ${COPY_DIR}/${BDATE}/${FILENAME##*/}
-                	cp "${FILENAME}.md5sum" ${COPY_DIR}/${BDATE}/${FILENAME##*/}.md5
+			echo "Moving to Copybox G"
+                	cp ${FILENAME} ${COPY_DIR}/${BDATE}/${BVARIANT}/${FILENAME##*/}
+                	cp "${FILENAME}.md5sum" ${COPY_DIR}/${BDATE}/${BVARIANT}/${FILENAME##*/}.md5sum
 			fi
 	        fi
         done
